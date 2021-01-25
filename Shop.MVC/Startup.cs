1b3aabdab165876e15fb1ddca3f46d8241b482c2
Dawid <jablonskidawid0202@gmail.com>
2021-01-25 21:48:01 +0100

    Add basic nuget package

--- Shop.MVC/Startup.cs
+++ Shop.MVC/Startup.cs
@@ -23,7 +23,7 @@ namespace Shop.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+           services.AddDbContext<ApplicationDbContext>
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
