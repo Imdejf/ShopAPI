a555b865f56c3060d0d5dd0fec9ba15211890ea0
Dawid <jablonskidawid0202@gmail.com>
2021-01-25 22:11:42 +0100

    Add DbContext

---
+++ Shop.DataAccess/ShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.DataAccess
+{
+    public class ShopDbContext : IdentityDbContext
+    {
+        public ShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Shop.MVC/Startup.cs
+++ Shop.MVC/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +26,13 @@ namespace Shop.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddDbContext<ApplicationDbContext>
+            services.AddDbContext<ShopDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ShopDbContext>();
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
