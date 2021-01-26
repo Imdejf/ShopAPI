d11bc87aeb1c227e88998f67117c5cd60038ab3d
Dawid <jablonskidawid0202@gmail.com>
2021-01-26 23:40:00 +0100

    Add UnitOfWork

---
+++ Shop.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Shop.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.DataAccess.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ShopDbContext _db;
+
+        public UnitOfWork(ShopDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
