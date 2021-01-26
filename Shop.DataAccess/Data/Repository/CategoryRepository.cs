69325c46bfa89ddeeabff2866e0331ef716a32f0
Dawid <jablonskidawid0202@gmail.com>
2021-01-26 23:39:09 +0100

    Add CategoryRepository

---
+++ Shop.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Shop.DataAccess.Data.Repository.IRepository;
+using System.Collections.Generic;
+using System.Linq;
+namespace Shop.DataAccess.Data.Repository
+{
+    public class CategoryRepository : Repository<Category> , ICategoryRepository
+    {
+        private readonly ShopDbContext _db;
+        
+        public CategoryRepository(ShopDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(s => new SelectListItem()
+            {
+                Text = s.Name,
+                Value = s.Id.ToString()
+            }); ;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+
+            _db.SaveChanges();
+        }
+    }
+}
