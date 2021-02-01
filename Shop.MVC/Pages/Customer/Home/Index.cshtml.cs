40e744f458610f641cb3d52d8ff22b3844200eb5
Dawid <jablonskidawid0202@gmail.com>
2021-02-01 23:22:18 +0100

    Create Home page

--- Shop.MVC/Pages/Customer/Home/Index.cshtml.cs
+++ Shop.MVC/Pages/Customer/Home/Index.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.DataAccess.Data.Repository.IRepository;
+using Shop.Models;
 
 namespace Shop.MVC.Pages.Customer.Home
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<MenuItem> MenuItemList { get; set; }
+        public IEnumerable<Category> CategoryList { get; set; }
+
         public void OnGet()
         {
+            MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
+            CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
         }
     }
 }
