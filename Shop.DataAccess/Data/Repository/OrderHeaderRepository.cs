6c7e22487e16ad82a9ce31b3fde904d32f28a9dc
Dawid <jablonskidawid0202@gmail.com>
2021-02-03 23:57:46 +0100

    Add OrderHeaderRepository class and interface

---
+++ Shop.DataAccess/Data/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Shop.DataAccess.Data.Repository.IRepository;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.DataAccess.Data.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ShopDbContext _db;
+        public OrderHeaderRepository(ShopDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderHeader orderHeader)
+        {
+            var orderHeaderFromDb = _db.OrderHeader.FirstOrDefault(m => m.Id == orderHeader.Id);
+
+            _db.OrderHeader.Update(orderHeaderFromDb);
+            _db.SaveChanges();
+
+        }
+    }
+}
