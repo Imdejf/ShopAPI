96d8f0b09b0ef08a4910276ee954f26956ab9df1
Dawid <jablonskidawid0202@gmail.com>
2021-02-03 23:56:39 +0100

    Add OrderDetailsRepository class and interface

---
+++ Shop.DataAccess/Data/Repository/IRepository/IOrderDetailsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.DataAccess.Data.Repository.IRepository
+{
+    public interface IOrderDetailsRepository : IRepository<OrderDetails>
+    {
+        void Update(OrderDetails orderDetails);
+    }
+}
---
+++ Shop.DataAccess/Data/Repository/OrderDetailsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Shop.DataAccess.Data.Repository.IRepository;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.DataAccess.Data.Repository
+{
+    public class OrderDetailsRepository : Repository<OrderDetails>, IOrderDetailsRepository
+    {
+        private readonly ShopDbContext _db;
+        public OrderDetailsRepository(ShopDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderDetails orderDetails)
+        {
+            var orderDeatilsFromDb = _db.OrderDetails.FirstOrDefault(m => m.Id == orderDetails.Id);
+            _db.OrderDetails.Update(orderDeatilsFromDb);
+
+            _db.SaveChanges();
+
+        }
+    }
+}
