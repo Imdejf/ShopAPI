143c1669f02b80139284881b626db73c5557275b
Dawid <jablonskidawid0202@gmail.com>
2021-02-05 23:59:10 +0100

    Add StripeSettings class

---
+++ Shop.Utility/StripeSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Utility
+{
+    public class StripeSetings
+    {
+        public string Secretkey { get; set; }
+        public string PublishableKey { get; set; }
+    }
+}
